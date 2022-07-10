1beb982b1949f0d7583c30da3112666d2dd3d1ea
Atwinenickson <atwiinenicxon@gmail.com>
2022-07-10 23:21:42 +0300

    add all api methods test cases

--- slimbackend/tests/Functional/indexApiTest.php
+++ slimbackend/tests/Functional/indexApiTest.php
@@ -15,4 +15,28 @@ class indexApiTest extends BaseTestCase
         $this->assertSame($response->getStatusCode(), 200);
         $result = json_decode($response->getBody(), true);
      }
+
+     public function testUsersGetOne()
+     {
+        $response=$this->runApp('GET', '/users/1');
+        $response = $this->app->run(true);
+        $this->assertSame($response->getStatusCode(), 200);
+        $result = json_decode($response->getBody(), true);
+     }
+
+     public function testUsersSignup()
+     {
+        $response=$this->runApp('POST', '/users/add');
+        $response = $this->app->run(true);
+        $this->assertSame($response->getStatusCode(), 200);
+        $result = json_decode($response->getBody(), true);
+     }
+
+     public function testUsersLogin()
+     {
+        $response=$this->runApp('POST', '/login');
+        $response = $this->app->run(true);
+        $this->assertSame($response->getStatusCode(), 200);
+        $result = json_decode($response->getBody(), true);
+     }
 }
